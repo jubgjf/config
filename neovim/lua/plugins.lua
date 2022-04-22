c3d0f1cbca2769ae7406858463fff86cc7bd2610
jubgjf <guanjiannan@outlook.com>
2022-04-22 11:48:30 +0800

    update

--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -83,7 +83,7 @@ end
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 
-local servers = { }
+local servers = { 'clangd' }
 for _, lsp in pairs(servers) do
   require("lspconfig")[lsp].setup({
     capabilities = capabilities,
