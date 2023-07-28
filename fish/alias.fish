f69b6451075e1304a5903b2450477410d5d93a1e
jubgjf <guanjiannan@outlook.com>
2023-07-29 00:09:12 +0800

    update fish

--- fish/alias.fish
+++ fish/alias.fish
@@ -13,11 +13,14 @@ alias vim "nvim"
 alias cat "bat"
 alias grep "rga"
 alias q "exit"
+alias r 'ranger --choosedir=$XDG_CACHE_HOME/rangerdir; LASTDIR=$(cat $XDG_CACHE_HOME/rangerdir); cd "$LASTDIR"'
+alias top "htop"
+alias unzipw "unzip -O cp936"
 
 switch (hostname)
-case "*MacBook*"
+case "hpc-login*" "gpu*" "ln01"
+    alias sq 'squeue -o "%.4i %.9P %.10j %.10u %.10T %.10M %.12l %.6D %7R %b" -u $USER'
+    alias si 'sinfo -N -o "%5N  %5t  %13C  %8O  %8e  %7m  %G"'
+    alias sa 'sacct -X --format="JobID%6, State%10, JobName%15, Elapsed%10, AllocTRES%80"'
 case "*"
-    alias top "htop"
-    alias unzipw "unzip -O cp936"
-    alias r 'ranger --choosedir=$XDG_CACHE_HOME/rangerdir; set LASTDIR $(cat $XDG_CACHE_HOME/rangerdir); cd "$LASTDIR"'
 end
--- fish/conda.fish
+++ fish/conda.fish
@@ -3,8 +3,8 @@ case "*MacBook*"
     if test -f /opt/homebrew/Caskroom/miniconda/base/bin/conda
         eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
     end
-case "hpc-server"
-case "WorkStation"
-case "gpu*"
 case "*"
+    if test -f ~/.miniconda/bin/conda
+        eval ~/.miniconda/bin/conda "shell.fish" "hook" $argv | source
+    end
 end
