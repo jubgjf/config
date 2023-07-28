df7991def56c4741d14fd48ee0d45e3f0ddf9576
jubgjf <guanjiannan@outlook.com>
2023-07-28 18:12:19 +0800

    update starship, zsh for new scir

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -18,23 +18,22 @@ alias cat="bat"
 alias grep="rga"
 alias q="exit"
 alias r='ranger --choosedir=$XDG_CACHE_HOME/rangerdir; LASTDIR=$(cat $XDG_CACHE_HOME/rangerdir); cd "$LASTDIR"'
+alias top="htop"
+alias unzipw="unzip -O cp936"
 
-if [[ "$(uname)" != "Darwin" ]] {
-    alias top="htop"
-    alias unzipw="unzip -O cp936"
-}
-
-if [[ "$(hostname)" == "hpc-server" || "$(hostname)" == gpu* ]] {
-    alias sq='squeue -o "%.i %.9P %.10j %.10u %.10T %.10M %.12l %.6D %R" -u $USER'
-    unalias grep
-    unalias vim
-    alias vim="~/.local/bin/nvim"
-} elif [[ "$(hostname)" == "zxk-Ubuntu22" ]] {
-    unalias grep
-    alias grep="rg"
+if [[ "$(hostname)" == hpc-login* || "$(hostname)" == gpu* ]] {
+    alias sq='squeue -o "%.4i %.9P %.10j %.10u %.10T %.10M %.12l %.6D %7R %b" -u $USER'
+    alias si='sinfo -N -o "%5N  %5t  %13C  %8O  %8e  %7m  %G"'
+    alias sa='sacct -X --format="JobID%6, State%10, JobName%15, Elapsed%10, AllocTRES%80"'
 } else {
     sq() {
-        ssh scir 'squeue -o "%.i %.9P %.10j %.10u %.10T %.10M %.12l %.6D %R" -u $USER'
+        ssh scir 'squeue -o "%.4i %.9P %.10j %.10u %.10T %.10M %.12l %.6D %7R %b" -u $USER'
     }
-    alias scir-watch="ssh scir scir-watch"
+    si() {
+        ssh scir 'sinfo -N -o "%5N  %5t  %13C  %8O  %8e  %7m  %G"'
+    }
+    sa() {
+        ssh scir 'sacct -X --format="JobID%6, State%10, JobName%15, Elapsed%10, AllocTRES%80"'
+    }
+    alias sw="ssh scir scir-watch"
 }
--- zsh/zellij.zsh
+++
@@ -1,7 +0,0 @@
-if [[ "$(hostname)" == "hpc-server" || "$(hostname)" == "zxk-Ubuntu22" ]] {
-    if [[ -z "$ZELLIJ" ]]; then
-        if [[ "$ZELLIJ_AUTO_ATTACH" == "true" ]]; then
-            zellij attach
-        fi
-    fi
-}
