33d913d97e46f53c93216bc02a6756f4520d5523
jubgjf <guanjiannan@outlook.com>
2023-08-02 14:49:38 +0800

    use `uname -n` to replace `hostname`

--- zsh/conda.zsh
+++ zsh/conda.zsh
@@ -1,6 +1,6 @@
 if [[ "$(uname)" == "Darwin" ]] {
     source /opt/homebrew/Caskroom/miniconda/base/etc/profile.d/conda.sh
-} elif [[ "$(hostname)" == "surfacebook3" ]] {
+} elif [[ "$(uname -n)" == "surfacebook3" ]] {
     source /opt/miniconda/etc/profile.d/conda.sh
 } else {
     source ~/.miniconda/etc/profile.d/conda.sh
