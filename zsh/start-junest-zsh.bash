a52e0efd4646662d6e7b10d442b9e80a592f7635
jubgjf <guanjiannan@outlook.com>
2023-08-03 04:23:31 +0800

    update zsh: add zsh start script for junest

---
+++ zsh/start-junest-zsh.bash
@@ -0,0 +1,9 @@
+# JuNest 初始化
+export PATH=$PATH:~/.junest/usr/bin_wrappers
+export PATH=$PATH:~/.local/share/junest/bin
+
+# zsh functions
+export FPATH=$FPATH:~/.junest/usr/share/zsh/functions/Calendar:~/.junest/usr/share/zsh/functions/Chpwd:~/.junest/usr/share/zsh/functions/Completion:~/.junest/usr/share/zsh/functions/Exceptions:~/.junest/usr/share/zsh/functions/MIME:~/.junest/usr/share/zsh/functions/Math:~/.junest/usr/share/zsh/functions/Misc:~/.junest/usr/share/zsh/functions/Newuser:~/.junest/usr/share/zsh/functions/Prompts:~/.junest/usr/share/zsh/functions/TCP:~/.junest/usr/share/zsh/functions/VCS_Info:~/.junest/usr/share/zsh/functions/Zftp:~/.junest/usr/share/zsh/functions/Zle
+
+# start zsh
+~/.junest/usr/bin_wrappers/zsh -l
