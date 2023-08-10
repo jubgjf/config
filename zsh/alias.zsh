55c26580ad2623344d203b9cba3a574783b00243
jubgjf <guanjiannan@outlook.com>
2023-08-10 16:04:28 +0800

    update zsh: update slurm alias

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -26,13 +26,13 @@ if [[ "$(uname -n)" == hpc-login* || "$(uname -n)" == gpu* || "$(uname -n)" == m
   alias sa='sacct -X --format="JobID%6, State%10, JobName%15, Elapsed%10, AllocTRES%80"'
 } else {
   sq() {
-    ssh scir 'squeue -o "%5i %9P %10j %10u %10T %10M %12l %6D %7R %b" -u $USER'
+    ssh scir-01 'squeue -o "%5i %9P %10j %10u %10T %10M %12l %6D %7R %b" -u $USER'
   }
   si() {
-    ssh scir 'sinfo -N -o "%5N  %5t  %13C  %8O  %8e  %7m  %G"'
+    ssh scir-01 'sinfo -N -o "%5N  %5t  %13C  %8O  %8e  %7m  %G"'
   }
   sa() {
-    ssh scir 'sacct -X --format="JobID%6, State%10, JobName%15, Elapsed%10, AllocTRES%80"'
+    ssh scir-01 'sacct -X --format="JobID%6, State%10, JobName%15, Elapsed%10, AllocTRES%80"'
   }
   alias sw="ssh scir scir-watch"
 }
