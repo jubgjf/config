def746eaa3d0097d64e72d78d560de8e9b2c7317
jubgjf <guanjiannan@outlook.com>
2023-08-30 14:58:17 +0800

    update zsh alias, update ideavim

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -16,17 +16,18 @@ alias q="exit"
 alias r='ranger --choosedir=$XDG_CACHE_HOME/rangerdir; LASTDIR=$(cat $XDG_CACHE_HOME/rangerdir); cd "$LASTDIR"'
 alias top="htop"
 alias unzipw="unzip -O cp936"
+alias ze="zellij"
 
 
 # ========== slurm ==========
 
 if [[ "$(uname -n)" == hpc-login* || "$(uname -n)" == gpu* || "$(uname -n)" == mngg001 || "$(uname -n)" == g400* || "$(uname -n)" == ln01 ]] {
-  alias sq='squeue -o "%5i %9P %10j %10u %10T %10M %12l %6D %7R %b" -u $USER'
+  alias sq='squeue -o "%6i %9P %10j %10u %10T %10M %12l %6D %10R %b" -u $USER'
   alias si='sinfo -N -o "%5N  %5t  %13C  %8O  %8e  %7m  %G"'
   alias sa='sacct -X --format="JobID%6, State%10, JobName%15, Elapsed%10, AllocTRES%80"'
 } else {
   sq() {
-    ssh scir-01 'squeue -o "%5i %9P %10j %10u %10T %10M %12l %6D %7R %b" -u $USER'
+    ssh scir-01 'squeue -o "%6i %9P %10j %10u %10T %10M %12l %6D %10R %b" -u $USER'
   }
   si() {
     ssh scir-01 'sinfo -N -o "%5N  %5t  %13C  %8O  %8e  %7m  %G"'
