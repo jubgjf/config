1ad6c81b3411857f2801f7a42632dc2928d79b07
jubgjf <guanjiannan@outlook.com>
2023-10-26 16:25:30 +0800

    update zsh: support hostname `mngg*`

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -21,7 +21,7 @@ alias ze="zellij"
 
 # ========== slurm ==========
 
-if [[ "$(uname -n)" == hpc-login* || "$(uname -n)" == gpu* || "$(uname -n)" == mngg001 || "$(uname -n)" == g400* || "$(uname -n)" == ln01 ]] {
+if [[ "$(uname -n)" == hpc-login* || "$(uname -n)" == gpu* || "$(uname -n)" == mngg* || "$(uname -n)" == g400* || "$(uname -n)" == ln01 ]] {
   alias sq='squeue -o "%6i %9P %10j %10u %10T %10M %12l %6D %10R %b" -u $USER'
   alias si='sinfo -N -o "%5N  %5t  %13C  %8O  %8e  %7m  %G"'
   alias sa='sacct -X --format="JobID%6, State%10, JobName%15, Elapsed%10, AllocTRES%80"'
