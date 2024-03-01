18349a22565801f0957239f834af86145c890c16
jubgjf <guanjiannan@outlook.com>
2024-03-01 20:43:45 +0800

    update zsh: update conda hostname

--- zsh/conda.zsh
+++ zsh/conda.zsh
@@ -2,7 +2,7 @@
 
 if [[ "$(uname)" == "Darwin" ]] {
   . /opt/homebrew/Caskroom/miniconda/base/etc/profile.d/conda.sh
-} elif [[ "$(uname -n)" == "guan-archlinux" ]] {
+} elif [[ "$(uname -n)" == "WorkStation" ]] {
   . /opt/miniconda/etc/profile.d/conda.sh
 } else {
   . $HOME/.miniconda/etc/profile.d/conda.sh
