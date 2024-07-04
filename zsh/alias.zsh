90ff5cba4a05f1c433589aabeb7de617722bf13e
jubgjf <guanjiannan@outlook.com>
2024-07-04 16:55:25 +0800

    add yazi

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -12,6 +12,14 @@ alias unzipw="unzip -O cp936"
 alias ze="zellij"
 alias diff="delta -s"
 
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		cd "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
 
 # ========== slurm ==========
 
